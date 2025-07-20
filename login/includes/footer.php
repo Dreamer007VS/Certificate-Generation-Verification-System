3145aa095985e1c7d3c2ae89937716072cfb258c
Dreamer007VS <65842599+Dreamer007VS@users.noreply.github.com>
2025-07-20 18:17:07 +0530

    Add files via upload

---
+++ login/includes/footer.php
@@ -0,0 +1,43 @@
+<p class="love" style="font-family: Georgia, 'Times New Roman', Times, serif; color: #1bb1dc;">Made with <i class="fa fa-heart" style="color: red;"></i> in CGV</p>
+</div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+
+
+    <!-- Bootstrap Core JavaScript -->
+
+        <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="assets/js/metisMenu/metisMenu.min.js"></script>
+
+
+
+
+    <!-- Custom Theme JavaScript -->
+    <script src="assets/js/sb-admin-2.js"></script>
+    <script src="assets/js/jquery.validate.min.js"></script>
+    <script src="assets/js/particles.min.js"></script>
+    <script src="assets/js/index.js"></script>
+    <script type="text/javascript"> 
+    function disableselect(e){  
+    return false  
+    }  
+    
+    function reEnable(){  
+    return true  
+    }  
+    
+    //if IE4+  
+    document.onselectstart=new Function ("return false")  
+    document.oncontextmenu=new Function ("return false")  
+    //if NS6  
+    if (window.sidebar){  
+    document.onmousedown=disableselect  
+    document.onclick=reEnable  
+    }
+    </script>
+</body>
+
+</html>
